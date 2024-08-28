6c539cf371babe6751e582332388fb71999ccf7d
Mai <mvtuan777@gmail.com>
2024-08-28 16:42:44 +0700

    custom plugin

--- lib/src/user_planner_user_time.dart
+++ lib/src/user_planner_user_time.dart
@@ -2,7 +2,7 @@ class UserPlannerUserTime {
   /// Day index from 0, this index dependence on your time planner header
   int day;
 
-  /// Task will be begin at this hour
+  /// Index in list user
   int index;
 
   UserPlannerUserTime({
